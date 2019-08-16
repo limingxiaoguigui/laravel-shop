046b98f6329a5abe94300bda876a82a5157976bc
LMG <htyzhliminggui@163.com>
2019-08-16 07:10:51 +0000

    订单详情

--- routes/web.php
+++ routes/web.php
@@ -43,5 +43,7 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::post('orders', 'OrdersController@store')->name('orders.store');
     //订单列表
     Route::get('orders', 'OrdersController@index')->name('orders.index');
+    //订单详情
+    Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
 });
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
