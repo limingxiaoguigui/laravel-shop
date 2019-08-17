9d70627e7abdcfdec6b249ad81cb892d15b1b7fd
LMG <htyzhliminggui@163.com>
2019-08-17 10:10:09 +0000

    完善支付后逻辑

---
+++ app/Listeners/UpdateProductSoldCount.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\OrderPaid;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Models\OrderItem;
+
+class UpdateProductSoldCount  implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * // Laravel 会默认执行监听器的 handle 方法，触发的事件会作为 handle 方法的参数
+     *
+     * @param  OrderPaid  $event
+     * @return void
+     */
+    public function handle(OrderPaid $event)
+    {
+        //从事事件对象中取出对应的订单
+        $order = $event->getOrder();
+        // 预加载商品数据
+        $order->load('items.product');
+        //循环遍历订单中的商品
+        foreach ($order->items as $item) {
+            $product = $item->product;
+            //计算对应商品的销量
+            $soldCount = OrderItem::query()
+                ->where('product_id', $product->id)
+                ->whereHas('order', function ($query) {
+                    $query->whereNotNull('paid_at');
+                })->sum('amount');
+            //更新商品的销量
+            $product->update(['sold_count' => $soldCount]);
+        }
+    }
+}
